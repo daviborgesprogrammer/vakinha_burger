ef9de447393d162876d31199e90a4c303f74c929
Davi Borges <borges.davi.cezar@gmail.com>
2024-05-06 16:59:06 -0300

    home screen implements

---
+++ lib/app/pages/home/home_controller.dart
@@ -0,0 +1,24 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:dw9delivery/app/repositories/products/products_repository.dart';
+
+import 'home_state.dart';
+
+class HomeController extends Cubit<HomeState> {
+  final ProductsRepository _productsRepository;
+  HomeController(this._productsRepository) : super(const HomeState.initial());
+
+  Future<void> loadProducts() async {
+    emit(state.copyWith(status: HomeStateStatus.loading));
+    try {
+      final products = await _productsRepository.findAllProducts();
+      emit(state.copyWith(status: HomeStateStatus.loaded, products: products));
+    } catch (e, s) {
+      log('Erro ao buscar produtos', error: e, stackTrace: s);
+      emit(state.copyWith(
+          status: HomeStateStatus.error,
+          errorMessage: 'Erro ao buscar produtos'));
+    }
+  }
+}
